9ffb167532b0b447bddf60269cb78b335900c013
Maxence Demougeot <maxence.demougeot1@gmail.com>
2024-02-21 16:17:10 +0100

    TP2 Equipement

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,6 +3,8 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -32,6 +34,31 @@ public class Romain {
 			parler("J'abandonne...");
 		}
 		assert(oldForce > this.force);
+	}
+	
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+		case 1:
+			if (equipements[0].equals(equipement)) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement + ".");
+			} else {
+				ajouterEquipement(equipement);
+			}
+			break;
+
+		default:
+			ajouterEquipement(equipement);
+			break;
+		}
+	}
+	
+	private void ajouterEquipement(Equipement equipement) {
+		equipements[nbEquipement] = equipement;
+		nbEquipement++;
+		System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
 	}
 	
 	public static void main(String[] args) {
