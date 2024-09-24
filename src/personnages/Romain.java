57b1beafa2bdb63764f2a4586448b764e5712c5d
Maxence Demougeot <maxence.demougeot1@gmail.com>
2024-09-24 09:45:45 +0200

    TP2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -4,9 +4,15 @@ public class Romain {
 	private String nom;
 	private int force;
 	
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
+
+	
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		//Invariant
+		assert force > 0;
 	}
 	
 	public String getNom() {
@@ -22,11 +28,46 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
+		// précondition
+		assert force > 0;
+		int oldForce = force;
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aïe");
 		} else {
 			parler("J'abandonne...");
 		}
+		// post condition la force à diminuer
+		assert force < oldForce;
 	}
+	
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+		case 1:
+			if (equipements[0].equals(equipement)) {
+				System.out.println("Le soldat " + nom + " possède déjà un "
+	+ equipement + ".");
+			} else {
+				ajouterEquipement(equipement);
+			}
+			break;
+
+		default:
+			ajouterEquipement(equipement);
+			break;
+		}
+	}
+
+	private void ajouterEquipement(Equipement equipement) {
+		equipements[nbEquipement] = equipement;
+		nbEquipement++;
+		System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+	}
+
+
+	
+	
 }
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,10 +3,27 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
+
 	
 	public Village(String nom, Chef chef) {
 		this.nom = nom;
 		this.chef = chef;
+	}
+	
+	public Village(String nom, int nbVillageoisMaximum) {
+		this.nom = nom;
+		villageois = new Gaulois[nbVillageoisMaximum];
+	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois++;
+	}
+	
+	public Gaulois trouverHabitant (int numGaulois) {
+		return villageois[numGaulois];
 	}
 	
 	public String getNom() {
@@ -16,4 +33,32 @@ public class Village {
 	public void setChef(Chef chef) {
 		this.chef = chef;
 	}
+	
+	public void afficherVillageois() {
+		System.out.println("Dans village du chef " + chef.getNom() + " vivent les légendaires gaulois : ");
+		for (int i = 0; i < nbVillageois; i++) {
+			System.out.println("- " + villageois[i].getNom());
+		}
+	}
+
+	
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles", 30);
+//		Gaulois gaulois = village.trouverHabitant(30);
+//		Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException
+//		Le tableau possede 30 cases numerote de 0 a 29
+		Chef chef = new Chef("Abraracourcix", 6, village);
+		village.setChef(chef);
+		Gaulois asterix = new Gaulois("Asterix", 8);
+		village.ajouterHabitant(asterix);
+		//Gaulois gaulois = village.trouverHabitant(1);
+		//System.out.println(gaulois);
+		
+		Gaulois obelix = new Gaulois("Obelix", 25);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois();
+		
+	}
+	
 }
